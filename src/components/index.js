c07123724dc370a918f366488468cb446d4daa40
Larissa Perinoto <perinotolarissa@gmail.com>
2022-10-17 14:16:02 -0300

    Adiciona index de exportação.

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as Filters } from './Filters';
+export { default as Search } from './Search';
+export { default as Table } from './Table';
